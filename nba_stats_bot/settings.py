6d95489779addff554923d5486e82936c866e3d7
Louis Tiao <louistiao@gmail.com>
2014-12-24 15:01:35 +0800

    start bot

---
+++ nba_stats_bot/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for nba_stats_bot project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'nba_stats_bot'
+
+SPIDER_MODULES = ['nba_stats_bot.spiders']
+NEWSPIDER_MODULE = 'nba_stats_bot.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'nba_stats_bot (+http://www.yourdomain.com)'
